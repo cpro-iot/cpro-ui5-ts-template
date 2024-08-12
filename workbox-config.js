3d0646f5499379a4f3a35f1a50b94bc2da1ad8b6
Tobias Quante <tobias.quante@cpro-iot.com>
2024-08-12 10:14:05 +0200

    docs: add todo comment for deployments

--- workbox-config.js
+++ workbox-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	// TODO: Change glob directory and swDest before deployment!
 	globDirectory: 'dist/',
 	globPatterns: [
 		'**/*.{ts,webmanifest,png,jpg,js,css,properties,html,json,md,xml,less,svg,gif,txt,ico,woff2,wasm,hpb}'
